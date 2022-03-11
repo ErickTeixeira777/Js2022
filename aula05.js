31c48808826a61949af71cd13f459485cea4546a
ErickTeixeira777 <contatoerickvenon@gmail.com>
2022-03-11 14:07:17 -0300

    Up date

--- aula05.js
+++ aula05.js
@@ -1,1 +1,8 @@
-//Functions
+//Functions Verbo ++ Substantivos
+let corSite = "azul"
+function resetaCor(cor,tonalidade){
+    corSite = cor +' '+ tonalidade;
+}; 
+console.log(corSite);
+resetaCor("vermelho","sangue");
+console.log(corSite);
